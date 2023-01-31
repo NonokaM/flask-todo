301f2290e5d8602ab4f04715a42936d3b2bef6eb
nonokaM <c1201071@gmail.com>
2023-01-31 20:52:59 +0900

    SQLite準備

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, request, render_template
-import codecs
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 @app.route("/")
-def top():
+def index():
     message = "test_message = OK"
 
-    return render_template("mainpage.html", message = message)
+    todo_list = Todo.quary.all()
+    print(todo_list)
+
+    return render_template("base.html", message = message)
+
+if __name__ == "__main__":
+    db.create_all()
+
+    new_todo = Todo(title="todo 1",compile=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
+    app.run(debug=True)
+
 
