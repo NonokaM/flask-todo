32b179da973abb63a0b2f2a7a54cb224c502af51
nonokaM <c1201071@gmail.com>
2023-06-23 12:00:00 +0900

    fix: db error

--- app.py
+++ app.py
@@ -6,18 +6,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
-
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -27,14 +24,12 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
-
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
-
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
@@ -43,14 +38,7 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
-
 if __name__ == "__main__":
-    # db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-# Flask,SQLAlchemy,create_all()ができないとき、対話シェルで下記の通り
-
-# from app import app,db
-
-# > with app.app_context():
-# ...     db.create_all()
